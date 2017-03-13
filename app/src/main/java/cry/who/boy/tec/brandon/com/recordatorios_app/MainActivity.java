f86b60a03363a0595c313eab4f5531effadb29e5
Brandon <colima_97@hotmal.com>
2017-03-12 20:13:44 -0600

    TextView

--- app/src/main/java/cry/who/boy/tec/brandon/com/recordatorios_app/MainActivity.java
+++ app/src/main/java/cry/who/boy/tec/brandon/com/recordatorios_app/MainActivity.java
@@ -2,12 +2,18 @@ package cry.who.boy.tec.brandon.com.recordatorios_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView campo_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campo_text=(TextView) findViewById(R.id.texto);
+        campo_text.setText("Hola de nuevo putito");
     }
 }
