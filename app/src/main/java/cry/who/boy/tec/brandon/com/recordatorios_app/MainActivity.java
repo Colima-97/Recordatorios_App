c2431de79555101a32bbef966c30947ad13dae73
Brandon Itzcoatl Pérez Sandoval <colima_97@hotmail.com>
2017-03-28 12:16:10 -0600

    Cree una nueva Activity cuando se pulsa el boton Lupa (Search)

--- app/src/main/java/cry/who/boy/tec/brandon/com/recordatorios_app/MainActivity.java
+++ app/src/main/java/cry/who/boy/tec/brandon/com/recordatorios_app/MainActivity.java
@@ -1,7 +1,9 @@
 package cry.who.boy.tec.brandon.com.recordatorios_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void onClick(View view){
+        Intent Search=new Intent(this, Search.class);
+        startActivity(Search);
+
+    }
+
 }
