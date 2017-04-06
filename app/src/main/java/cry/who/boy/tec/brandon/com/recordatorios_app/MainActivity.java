bc5977a8c51e471f78cacf47e7298f5af6766040
Brandon Pérez Sandoval <Brandon Prez Sandoval>
2017-04-05 21:03:33 -0500

    Cambié Spinners por EditText Date

--- app/src/main/java/cry/who/boy/tec/brandon/com/recordatorios_app/MainActivity.java
+++ app/src/main/java/cry/who/boy/tec/brandon/com/recordatorios_app/MainActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -20,5 +24,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(Buscar);
 
     }
+
+
 
 }
